98a8e101c84171cd92bb5eeacf246cad79098df4
Eric Pascal <eric@pascal-lab.ch>
2023-07-21 12:36:56 +0200

    ajout de log

--- build/src/plugin.js
+++ build/src/plugin.js
@@ -3,6 +3,8 @@ export default (config = {}) => {
     return {
         name: 'obfuscate-glsl',
         transform(src, id) {
+            if (id.indexOf('glsl') >= 0)
+                console.log(`glsl processing ${id}`);
             if (!this.meta.watchMode && config.shaderMinifier && id.endsWith('.glsl?raw')) {
                 const glslCode = JSON.parse(src.replace('export default', ''));
                 const cmd = spawnSync(config.shaderMinifier, ['--format', 'text', '--preserve-externals', '-o', '-', id.replace('?raw', '')]);
@@ -10,8 +12,8 @@ export default (config = {}) => {
                     return `export default ${JSON.stringify(cmd.stdout.toString())}`;
                 }
                 else {
-                    console.log('Error');
-                    return 'Error';
+                    console.log(`Error in GLSL conversion ${id}\n`);
+                    return `Error in GLSL conversion ${id}`;
                 }
             }
         },
--- src/plugin.ts
+++ src/plugin.ts
@@ -9,7 +9,9 @@ export default (config: PluginConfig = {}): Plugin => {
   return {
     name: 'obfuscate-glsl',
     transform(src, id) {
+      if (id.indexOf('glsl') >= 0 ) console.log(`glsl processing ${id}`);
       if (!this.meta.watchMode && config.shaderMinifier && id.endsWith('.glsl?raw')) {
+        // console.log('Processing '+id);
         const glslCode = JSON.parse(src.replace('export default', ''));
         // console.log(id, config.shaderMinifier)
         const cmd = spawnSync(config.shaderMinifier, [ '--format', 'text', '--preserve-externals', '-o', '-', id.replace('?raw', '') ]);
@@ -17,8 +19,8 @@ export default (config: PluginConfig = {}): Plugin => {
           // console.log('\nResult GLSL ', cmd.stdout.toString());
           return `export default ${JSON.stringify(cmd.stdout.toString())}`;
         } else {
-          console.log('Error');
-          return 'Error'
+          console.log(`Error in GLSL conversion ${id}\n`);
+          return `Error in GLSL conversion ${id}`;
         }
       }
     },
